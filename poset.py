0765d1a539ecdb18b671673dcf936e3f9667cb21
altugkarakurt <altugkarakurt@gmail.com>
2024-11-04 21:18:05 -0700

    Clarification to the previous commit

--- poset.py
+++ poset.py
@@ -14,11 +14,15 @@ class Poset:
     labels   : (Optional) Labels for elements, aligned with elems list.
                Default is str(elem)
     poset_str: (Optional) A short-hand label for the poset.
-               Default is '{elems,}'
+               Default is '{elems , cmp_func}'
     ----------------------------------------------------------------"""
     def __init__(self, elems, cmp_func=None, labels=None, poset_str=None):
 
-        if(isinstance(elems[0], Iterable)):
+        # This flag enables/disables order theoretic functionality such as
+        # up-/down-set util functions. The elements in this poset are sets
+        self.set_of_sets = isinstance(elems[0], Iterable)
+
+        if(self.set_of_sets):
             self.elems = [set(s) for s in elems]
             self.labels = labels if(labels is not None) \
                           else [f"{{{','.join([str(si) for si in s])}}}" for s in elems]
@@ -40,7 +44,7 @@ class Poset:
     # Generate the elements as the powerset of a set of elements. If nonempty,
     # exclude the emptyset from the powerset
     @staticmethod
-    def from_base_set(base_set, cmp_func=None, labels=None, nonempty=True):
+    def from_base_set(base_set, nonempty=True):
         elems = [] if(nonempty) else [[]]
         elems.extend(powerset(base_set))
-        return Poset(elems, poset_str=(f"Poset({base_set})"))
+        return Poset(elems, poset_str=(f"{{Powerset({','.join([str(s) for s in base_set])}), Subset-Order}}"))
